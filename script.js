6198e7f49ea32310292a79872cc046764cba0a4c
Felipe Araya <felipe.araya.becerra@gmail.com>
2024-07-07 02:20:25 -0400

    Estilos Agregados y funcionalidad de copiar texto

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 function encriptarTexto() {
-    const llaves = {
-        "e": "enter",
-        "i": "imes",
-        "a": "ai",
-        "o": "ober",
-        "u": "ufat"
-    };
+  const llaves = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
 
-    let inputText = document.getElementById("inputText").value;
-    let outputText = "";
+  let inputText = document.getElementById("inputText").value.toLowerCase();
+  let outputText = "";
 
-    for (let i = 0; i < inputText.length; i++) {
-        let letra = inputText[i];
-        outputText += llaves[letra] ? llaves[letra] : letra;
-    }
+  for (let i = 0; i < inputText.length; i++) {
+    let letra = inputText[i];
+    outputText += llaves[letra] ? llaves[letra] : letra;
+  }
 
-    document.getElementById("outputText").value = outputText;
+  document.getElementById("outputText").value = outputText;
 }
 
 function desencriptarTexto() {
-    const llaves = {
-        "enter": "e",
-        "imes": "i",
-        "ai": "a",
-        "ober": "o",
-        "ufat": "u"
-    };
+  const llaves = {
+    enter: "e",
+    imes: "i",
+    ai: "a",
+    ober: "o",
+    ufat: "u",
+  };
 
-    let inputText = document.getElementById("inputText").value;
-    let outputText = inputText;
+  let inputText = document.getElementById("inputText").value.toLowerCase();
+  let outputText = inputText;
 
-    for (let key in llaves) {
-        let regex = new RegExp(key, "g");
-        outputText = outputText.replace(regex, llaves[key]);
-    }
+  for (let key in llaves) {
+    let regex = new RegExp(key, "g");
+    outputText = outputText.replace(regex, llaves[key]);
+  }
 
-    document.getElementById("outputText").value = outputText;
+  document.getElementById("outputText").value = outputText;
+}
+
+function copiarTexto() {
+  let outputText = document.getElementById("outputText");
+  outputText.select();
+  outputText.setSelectionRange(0, 9999);
+  document.execCommand("copy");
+  alert("Texto copiado: " + outputText.value);
 }
